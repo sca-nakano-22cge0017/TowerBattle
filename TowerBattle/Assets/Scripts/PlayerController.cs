68e49f28323112620289c5188b859d2bffb38125
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2023-04-22 12:23:02 +0900

    メインゲーム背景追加

---
+++ TowerBattle/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Transform playerTransform = this.transform;
+        Vector2 pos = playerTransform.position;
+        if(Input.GetKey(KeyCode.A))
+        {
+            pos.x -= moveSpeed * Time.deltaTime;
+            playerTransform.position = pos;
+        }
+        if(Input.GetKey(KeyCode.D)) {
+            pos.x += moveSpeed * Time.deltaTime;
+            playerTransform.position = pos;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        /*if(collision.WithTag("Ground"))
+        {
+            Debug.Log("衝突");
+        }*/
+        Debug.Log("syoutotu");
+    }
+}
