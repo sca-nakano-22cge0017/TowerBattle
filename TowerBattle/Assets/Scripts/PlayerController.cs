037191952153e4ad01201d9cfa26575bc9120d5f
NakanoAyame <sdt22cge0017@edu.sca.ac.jp>
2023-05-01 01:03:09 +0900

    プレハブ作成

--- TowerBattle/Assets/Scripts/PlayerController.cs
+++ TowerBattle/Assets/Scripts/PlayerController.cs
@@ -6,19 +6,48 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5;
     Rigidbody2D rbody;
-    bool isKey;
+    bool isKey, isSpown;
+
+    [SerializeField] GameObject[] obj;
+    Rigidbody2D objBody0, objBody1, objBody2, objBody3, objBody4, objBody5;
+    int ran, num;
 
     void Start()
     {
-        rbody = gameObject.GetComponent<Rigidbody2D>();
+        ran = Random.Range(0, obj.Length);
+        Instantiate(obj[ran], new Vector3(0, 3.5f, 0), Quaternion.identity);
+
+        rbody = this.GetComponent<Rigidbody2D>();
         rbody.simulated = false;
+
+        num = obj.Length;
+        /*for(int i = 0; i <= num; i++)
+        {
+            objBody[i] = obj[i].GetComponent<Rigidbody2D>();
+            objBody[i].simulated = false;
+        }*/
+        objBody0 = obj[0].GetComponent<Rigidbody2D>();
+        objBody1 = obj[1].GetComponent<Rigidbody2D>();
+        objBody2 = obj[2].GetComponent<Rigidbody2D>();
+        objBody3 = obj[3].GetComponent<Rigidbody2D>();
+        objBody4 = obj[4].GetComponent<Rigidbody2D>();
+        objBody5 = obj[5].GetComponent<Rigidbody2D>();
+        objBody0.simulated = false;
+        objBody1.simulated = false;
+        objBody2.simulated = false;
+        objBody3.simulated = false;
+        objBody4.simulated = false;
+        objBody5.simulated = false;
+
         isKey = false;
+        isSpown = false;
     }
 
     void Update()
     {
         Transform playerTransform = this.transform;
         Vector2 pos = playerTransform.position;
+
         if(Input.GetKey(KeyCode.A))
         {
             pos.x -= moveSpeed * Time.deltaTime;
@@ -31,16 +60,24 @@ public class PlayerController : MonoBehaviour
         if(Input.GetKey(KeyCode.Return))
         {
             isKey = true;
+            isSpown = true;
         }
         if(isKey)
         {
             rbody.simulated = true;
+            if(isSpown)
+            {
+                ran = Random.Range(0, obj.Length);
+                Instantiate(obj[ran], new Vector3(0, 3.5f, 0), Quaternion.identity);
+                isSpown = false;
+            }
+
             isKey = false;
         }
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnColliderEnter2D(Collision2D collision)
     {
-        
+
     }
 }
